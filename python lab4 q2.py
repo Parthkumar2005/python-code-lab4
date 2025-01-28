e7924110b25840db38bb6b32390de0586bbe6a13
Parthkumar2005 <1226parthkumar@gmail.com>
2025-01-28 23:06:07 +0530

    Add files via upload
    Q2) WAP to demonstrate built in functions of Strings.

---
+++ python lab4 q2.py
@@ -0,0 +1,10 @@
+string = "  Hello, World!  "
+print(string.strip())
+print(string.upper())
+print(string.lower())
+print(string.replace("World", "Python"))
+print(string.count("l"))
+print(string.find("World"))
+print(string.split())
+words = ['Hello', 'Python']
+print(" ".join(words))
